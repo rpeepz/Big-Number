b3979d7b451671574cb634c8fae3cb7abcaac78e
rpeepz <r.papagna@gmail.com>
2021-11-23 14:37:51 -0500

    update header comments to doc format

--- libbignum.h
+++ libbignum.h
@@ -16,17 +16,14 @@
 # include <stddef.h>
 # include <stdlib.h>
 
-# define ADD 1
-# define SUBTRACT 0
-
-/*
-	Custom data structure to handle whole numbers of arbitrary length
+/**
+	* Custom data structure to handle whole numbers of arbitrary length
 	*
-	char* number - string form of value
-	size_t alloc_size - size of memory allocated for 'number'
-	size_t len - digit length of 'number'
-	int sign - set to 1 if negative, 0 if positive
-*/
+	* @param	char* number - string form of value
+	* @param	size_t alloc_size - size of memory allocated for 'number'
+	* @param	size_t len - digit length of 'number'
+	* @param	int sign - set to 1 if negative, 0 if positive
+**/
 typedef struct		s_bignum
 {
 	char*			number;
@@ -41,21 +38,21 @@ typedef struct		s_bignum
 --------------------------------------------------------------------------------
 */
 
-/*
-	Stores pasred 'number' in an allocated pointer to struct 'bignum'.
-	Error input will truncate to last valid digit, or 0.
+/**
+	* Stores pasred 'number' into 'bignum' struct.
+	* Error input will truncate to last valid digit, or 0.
 	*
-	** Must be matched with 'bignum_del' to properly free memory *
-
-	number - number represented as a string.
-*/
+	* Must be matched with 'bignum_del' to properly free memory
+	* @param 	number number represented as a string.
+	* @returns 	An allocated pointer to struct 'bignum'.
+**/
 t_bignum*			bignum_init(char* number);
 
-/*
-	Frees 'bignum' struct along with the data inside, and sets to 0.
+/**
+	* Frees 'bignum' struct along with the data inside, and sets to 0.
 	*
-	p - reference to a 'bignum' pointer
-*/
+	* @param p	reference to a 'bignum' pointer
+**/
 void				bignum_del(t_bignum **p);
 
 /*
@@ -64,36 +61,40 @@ void				bignum_del(t_bignum **p);
 --------------------------------------------------------------------------------
 */
 
-/*
-	Adds a2 to a1, and returns an allocated pointer to the sum.
+/**
+	* Adds a2 to a1.
 	*
-	a1 - pointer to the first addend
-	a2 - pointer to the second addend
-*/
+	* @param	a1 pointer to the first addend.
+	* @param	a2 pointer to the second addend.
+	* @returns	An allocated pointer to the sum.
+**/
 t_bignum*			bignum_add(t_bignum* a1, t_bignum* a2);
 
-/*
-	Subtracts s from m, and returns an allocated pointer to the difference.
+/**
+	* Subtracts s from m.
 	*
-	m - pointer to the minuend
-	s - pointer to the subtrahend
-*/
+	* @param	m pointer to the minuend.
+	* @param	s pointer to the subtrahend.
+	* @returns	An allocated pointer to the difference.
+**/
 t_bignum*			bignum_minus(t_bignum* m, t_bignum* s);
 
-/*
-	Multiplies m1 by m2, and returns an allocated pointer to the product.
+/**
+	* Multiplies m1 by m2.
 	*
-	m1 - pointer to the multiplicand
-	m2 - pointer to the multiplier
-*/
+	* @param	m1 pointer to the multiplicand.
+	* @param	m2 pointer to the multiplier.
+	* @returns	An allocated pointer to the product.
+**/
 t_bignum*			bignum_multiply(t_bignum* m1, t_bignum* m2);
 
-/*
-	Divides d1 by d2, and returns an allocated pointer to the quotient.
+/**
+	* Divides d1 by d2.
 	*
-	d1 - pointer to the dividend
-	d2 - pointer to the divisor
-*/
+	* @param	d1 pointer to the dividend.
+	* @param	d2 pointer to the divisor.
+	* @returns	An allocated pointer to the quotient.
+**/
 t_bignum*			bignum_divide(t_bignum* d1, t_bignum* d2);
 
 /*
@@ -102,30 +103,30 @@ t_bignum*			bignum_divide(t_bignum* d1, t_bignum* d2);
 --------------------------------------------------------------------------------
 */
 
-/*
-	Helper Function for comparisons
-*/
+/**
+	* Helper Function for comparisons
+**/
 int					bignum_compare(t_bignum* n1, t_bignum* n2, int type);
 
-/*
-	Compares n1 to n2.
+/**
+	* Compares n1 to n2.
 	*
-	Returns 0 if n1 is greater.
-*/
+	* @returns	0 if n1 is greater.
+**/
 int					bignum_gt(t_bignum* n1, t_bignum* n2);
 
-/*
-	Compares n1 to n2
+/**
+	* Compares n1 to n2.
 	*
-	Returns 0 if n1 is lesser.
-*/
+	* @returns	0 if n1 is lesser.
+**/
 int					bignum_lt(t_bignum* n1, t_bignum* n2);
 
-/*
-	Compares n1 to n2
+/**
+	* Compares n1 to n2.
 	*
-	Returns 0 if n1 is equal to n2.
-*/
+	* @returns	0 if n1 is equal to n2.
+**/
 int					bignum_eq(t_bignum* n1, t_bignum* n2);
 
 /*
@@ -134,16 +135,16 @@ int					bignum_eq(t_bignum* n1, t_bignum* n2);
 --------------------------------------------------------------------------------
 */
 
-/*
-	Removes excess of preceding zeros from given input
+/**
+	* Removes excess of preceding zeros from given input.
 	*
-	Returns a pointer to the struct
-*/
+	* @returns	A pointer to the modified struct.
+**/
 t_bignum*			bignum_trunc(t_bignum* bignum);
 
-/*
-	Swaps data of 'a' and 'b'
-*/
+/**
+	* Swaps data of 'a' and 'b'.
+**/
 void			bignum_swap(t_bignum* a, t_bignum* b);
 
 #endif
