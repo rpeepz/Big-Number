a8775b22d8235c37fbfd4f97b360821183079871
rpeepz <r.papagna@gmail.com>
2021-11-23 12:53:15 -0500

    remove junk

--- bignum_gt.c
+++ bignum_gt.c
@@ -15,25 +15,4 @@
 int		bignum_gt(t_bignum *n1, t_bignum *n2)
 {
 	return (bignum_compare(n1, n2, 0));
-	// size_t	i;
-
-	// if (n1->sign == 0 && n2->sign == 1)
-	// 	return (0);
-	// if ((n1->sign == 1 && n2->sign == 0) || !bignum_eq(n1, n2))
-	// 	return (1);
-	// if (n1->len > n2->len)
-	// 	return (n1->sign == 0 ? 0 : 1);	
-	// if (n1->len < n2->len)
-	// 	return (n1->sign == 1 ? 0 : 1);	
-	// i = 0;
-	// while (i < n1->len)
-	// {
-	// 	if (n1->number[i] == n2->number[i])
-	// 		++i;
-	// 	else if (n1->number[i] > n2->number[i])
-	// 		return (n1->sign == 0 ? 0 : 1);
-	// 	else
-	// 		return (n1->sign == 1 ? 0 : 1);
-	// }
-	// return (0);
 }
--- bignum_lt.c
+++ bignum_lt.c
@@ -14,27 +14,5 @@
 
 int		bignum_lt(t_bignum *n1, t_bignum *n2)
 {
-	
 	return (bignum_compare(n1, n2, 1));
-	// size_t	i;
-
-	// if (n1->sign == 1 && n2->sign == 0)
-	// 	return (0);
-	// if ((n1->sign == 0 && n2->sign == 1) || !bignum_eq(n1, n2))
-	// 	return (1);
-	// if (n1->len > n2->len)
-	// 	return (n1->sign == 0 ? 1 : 0);	
-	// if (n1->len < n2->len)
-	// 	return (n1->sign == 1 ? 1 : 0);	
-	// i = 0;
-	// while (i < n1->len)
-	// {
-	// 	if (n1->number[i] == n2->number[i])
-	// 		++i;
-	// 	else if (n1->number[i] > n2->number[i])
-	// 		return (n1->sign == 0 ? 1 : 0);
-	// 	else
-	// 		return (n1->sign == 1 ? 1 : 0);
-	// }
-	// return (0);
 }
